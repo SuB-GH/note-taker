ed677ccfcb5a87967f910b40fb429e284134821f
Susan Brown <brown684408@gmail.com>
2022-08-07 20:44:10 -0500

    integrate front-end and back-end

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
+
 const fs = require('fs');
 const path = require('path');
 
@@ -9,20 +12,22 @@ const app = express()
 app.use(express.urlencoded({ extended: true }));
 // parse JSON data
 app.use(express.json());
-// app.use('/api', apiRoutes);
-// app.use('/', htmlRoutes);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.use(express.static('public'));
+
 const { notes } = require('./db/db.json')
+//const notes = require('./db/db.json')
 
 //const router = express('express').Router;
 
 // This will accept form input from the index.html file
-app.post('/api/notes', (req, res) => {
-  // req.body is where our incoming content will be
-  console.log(req.body);
-  res.json(req.body);
-});
+// app.post('/api/notes', (req, res) => {
+//   // req.body is where our incoming content will be
+//   console.log(req.body);
+//   res.json(req.body);
+// });
 
 // HTML Routes to request data from front-end
 // GET /notes should return the notes.html file.
@@ -31,14 +36,17 @@ app.post('/api/notes', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../Develop/public/notes.html'));
 // });
 
-app.get('/notes', (req, res) => {
-  res.json(notes);
-});
+// app.get('/notes', (req, res) => {
+//   res.json(notes);
+// });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../public/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../../public/index.html'));
+// });
 
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../../public/notes.html'));
+// });
 
 
 
@@ -47,15 +55,15 @@ app.get('*', (req, res) => {
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved 
 // (look into npm packages that could do this for you).
 
-app.get('/db', (req, res) => {
-  res.sendFile(path.join(__dirname, './db/db.json'));
-  res.send('Hello!');
-});
-module.exports = app;
+// app.get('/db', (req, res) => {
+//   res.sendFile(path.join(__dirname, './db/db.json'));
+//   res.send('Hello!');
+// });
+// module.exports = app;
 
-app.get('/db/db', (req, res) => {
-  res.send('Hello!');
-});
+// app.get('/db/db', (req, res) => {
+//   res.send('Hello!');
+// });
 
 app.listen(3100, () => {
     console.log(`API server now on port 3100!`);
