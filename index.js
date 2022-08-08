d617cc91ac41eade5c163a03c5016e8130d83b9a
Susan Brown <brown684408@gmail.com>
2022-08-08 12:12:03 -0500

    add api routes

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('../apiRoutes/routeIndex.js');
+const htmlRoutes = require('../htmlRoutes');
 
 const fs = require('fs');
 const path = require('path');
 
-const express = require('express');
+const express = require('express');  //this "requires" express.js
 const PORT = process.env.PORT || 3100;
-const app = express()
+const app = express()  //this instantiates the server
 
 // parse data
 app.use(express.urlencoded({ extended: true }));
@@ -17,7 +17,10 @@ app.use('/', htmlRoutes);
 
 app.use(express.static('public'));
 
+// this "requires" the notes data to be handled by the route below
 const { notes } = require('./db/db.json')
+
+
 //const notes = require('./db/db.json')
 
 //const router = express('express').Router;
@@ -61,10 +64,12 @@ const { notes } = require('./db/db.json')
 // });
 // module.exports = app;
 
-// app.get('/db/db', (req, res) => {
-//   res.send('Hello!');
-// });
+// route for note request
+app.get('/db/db', (req, res) => {
+  res.send('Hello!');
+});
 
+// this allows us to chain on methods to the Express.js server
 app.listen(3100, () => {
     console.log(`API server now on port 3100!`);
   });
--- routes/apiRoutes/routeIndex.js
+++ routes/apiRoutes/routeIndex.js
@@ -1,7 +1,44 @@
 const router = require('express').Router();
-const noteRoutes = require('../apiRoutes/noteRoutes');
+const noteRoutes = require('../apiRoutes/routeIndex.js');
 
 router.use(noteRoutes);
 
 module.exports = router;
-router.use(require('./noteRoutes'));
+//router.use(require('./noteRoutes'));
+
+
+const { filterByQuery, findById, createNewNote, validateNote } = require('../../lib/notes');
+const { notes } = require('../../data/notes');
+
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    if (req.query) {
+        results = filterByQuery(req.query, results);
+    }
+    res.json(results);
+});
+
+router.get('/notes/:id', (req, res) => {
+    const result = findById(req.params.id, notes);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
+});
+
+router.post('/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  
+    // if any data in req.body is incorrect, send 400 error back
+    if (!validateNote(req.body)) {
+      res.status(400).send('The note is not properly formatted.');
+    } else {
+      const note = createNewNote(req.body, notes);
+      res.json(note);
+    }
+  });
+
+  module.exports = router;
