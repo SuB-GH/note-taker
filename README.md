acf1bc7f5ef680b871192eb4c1c91cdf494f31bf
Susan Brown <brown684408@gmail.com>
2022-08-10 21:24:14 -0500

    finalize README

--- README.md
+++ README.md
@@ -3,12 +3,6 @@
 ## Description: 
 This note taker app allows the user to create and save notes.  It is a very useful tool for keeping track of tasks, reminders, and other types of notes.  Displaying items as a list helps the user stay organized.
 
-## Installation:
-
-## Usage:
-
-## Features:
-
 ## Screenshot:
 
 ![screenshot of notes page](public/assets/images/Capture2.PNG)
@@ -18,3 +12,4 @@ This note taker app allows the user to create and save notes.  It is a very usef
 This application has been deployed to Heroku: 
 https://evening-spire-55005.herokuapp.com/
 
+## Created by:  Susan
--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,59 +1,20 @@
 const router = require('express').Router();
-// const index = require('../apiRoutes/index'); //testing this out
-// router.use(index);
 
 const { createNewNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
 
-
-// THIS WORKS!
 router.get('/db', (req, res) => {
-    let results = notes;
-    // if (req.query) {
-    //   results = filterByQuery(req.query, results);
-    // } try adding this if it works?
-    res.json(results);
-    console.log("test")
+  let results = notes;
+  res.json(results);
 });
 
-// this works in insomnia, but not in browser!!
 router.post('/db', (req, res) => {
   // set id based on what the next index of the array will be
   req.body.id = notes.length.toString();
-  console.log("test????")
-  // if (!validateNote(req.body)) {
-  //   res.status(400).send('The note is not properly formatted.');
-  // } else {
-    const note = createNewNote(req.body, notes);
-    res.json(note);
+
+  const note = createNewNote(req.body, notes);
+  res.json(note);
   //}
-  console.log("test????")
 });
 
-  // router.post('/note-taker/db/db.json', (req, res) => {
-  //   // set id based on what the next index of the array will be
-  //   req.body.id = animals.length.toString();
-  
-  //   // if any data in req.body is incorrect, send 400 error back
-  //   if (!validateAnimal(req.body)) {
-  //     res.status(400).send('The animal is not properly formatted.');
-  //   } else {
-  //     const animal = createNewAnimal(req.body, animals);
-  //     res.json(animal);
-  //   }
-  //   console.log("is this one working?");
-  // });
-
 module.exports = router;
-
-
-
-//router.get('/notes/:id', (req, res) => { testing line 17
-//     router.get('/db', (req, res) => {
-//     const result = findById(req.params.id, notes);
-//     if (result) {
-//         res.json(result);
-//     } else {
-//         res.send(404);
-//     }
-// });
