984cdf071bff8fbd1bfaf0a4bbca49e6f4294a73
Susan Brown <brown684408@gmail.com>
2022-08-13 12:17:24 -0500

    fix routes for heroku

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -6,11 +6,9 @@ router.get('/', (req, res) => {
 });
 
 //GET /notes returns the notes.html file.
-
 router.get('/notes', (req, res) => {
-  console.log(path.join(__dirname, '../public/notes.html'));
   res.sendFile(path.join(__dirname, '../../public/notes.html'));
-  });
+});
 
 router.get('/index', (req, res) => {
   res.sendFile(path.join(__dirname, '../../public/index.html'));
