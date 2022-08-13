60a6781d95ffb6f67b9aaa1fdd5534715d76a591
Susan Brown <brown684408@gmail.com>
2022-08-13 11:51:56 -0500

    fix paths

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -2,21 +2,23 @@ const path = require('path');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../note-taker/public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 //GET /notes returns the notes.html file.
+
 router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../note-taker/public/notes.html'));
-});
+  console.log(path.join(__dirname, '../public/notes.html'));
+  res.sendFile(path.join(__dirname, '../../public/notes.html'));
+  });
 
 router.get('/index', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../note-taker/public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 //GET * returns the index.html file.
 router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../../note-taker/public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
 module.exports = router;
